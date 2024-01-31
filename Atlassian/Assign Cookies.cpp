1d1543c9c06c0b7bba42bf09e88b0c8e300c3e7b
ShivamShah12 <112316509+Shivam-shah12@users.noreply.github.com>
2024-01-31 07:39:18 -0800

    Create Assign Cookies.cpp

---
+++ Atlassian/Assign Cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+         sort(s.begin(),s.end());
+        
+        int count=0,i=0,j=0;
+        while(i<g.size() && j<s.size())
+        {
+            if(s[j]>=g[i])
+            {   
+                count++;
+                i++;
+                j++;
+            }
+            else if (s[j]<g[i])
+                j++;
+            
+        }
+        return count;
+    }
+};
